c2489ad48554a2a770f2ce3474e3c27720d94ac6
Nobuo Yamashita <nobsun@sampou.org>
2025-12-01 10:45:30 +0900

    start project

---
+++ test/doctests.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GHC2024 #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE LambdaCase, MultiWayIf #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE DataKinds, PolyKinds, NoStarIsType, TypeFamilyDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot, NoFieldSelectors, DuplicateRecordFields #-}
+module Main where
+
+import Test.DocTest
+
+main :: IO ()
+main = doctest ["src/Lib.hs"]
